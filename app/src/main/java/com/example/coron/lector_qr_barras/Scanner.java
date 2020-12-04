2f484aa4caabe22d8f2747611bd5bc70160841b6
Christofer96ACM <coronado_chris@hotmail.com>
2020-12-04 00:28:35 -0500

    2

--- app/src/main/java/com/example/coron/lector_qr_barras/Scanner.java
+++ app/src/main/java/com/example/coron/lector_qr_barras/Scanner.java
@@ -1,5 +1,4 @@
 package com.example.coron.lector_qr_barras;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,9 +10,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +24,7 @@ import com.google.zxing.integration.android.IntentResult;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import java.util.HashSet;
@@ -33,37 +34,28 @@ import java.util.Set;
 public class Scanner extends AppCompatActivity {
 
     SharedPreferences preference;
-
     private Button btnScanner;
+    private Button btnEnvioLote;
     private TextView tvBarCode;
-
     private TextView tvCodigo;
     DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mRootChild = mDatabaseReference.child("Codigo");
-
+    DatabaseReference mRootChild = mDatabaseReference.child("Numero_Lote").child("Numero");
     private ListView listScannedQRs;
-
     private Set<String> scannedQRs;
-
-
+    String CodigoLeido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
         btnScanner = findViewById(R.id.btnScanner);
+        btnEnvioLote = findViewById(R.id.btnEnvioLote);
         tvBarCode = findViewById(R.id.tvBarCode);
         tvCodigo = findViewById(R.id.tvCodigo);
         btnScanner.setOnClickListener(mOnClickListener);
+        btnEnvioLote.setOnClickListener(mOnClickListener);
         listScannedQRs = findViewById(R.id.lIstScannedQR);
-
-        preference = getSharedPreferences("STORE", Context.MODE_PRIVATE);
-        scannedQRs = preference.getStringSet("scannedQRs", new HashSet<String>());
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, scannedQRs.toArray( new String[scannedQRs.size()]) );
-        listScannedQRs.setAdapter(adapter);
-
-        Log.i("SCANNED-QRS", "onCreate: " + scannedQRs);
+        Update_List();
     }
 
     @Override
@@ -72,17 +64,16 @@ public class Scanner extends AppCompatActivity {
         mRootChild.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String CodigoLeido = dataSnapshot.getValue().toString();
-                tvBarCode.setText(CodigoLeido);
+                //String Num_Lote = dataSnapshot.getValue().toString();
+                CodigoLeido = dataSnapshot.getValue().toString();
+                tvBarCode.setText("Lote Numero: "+ CodigoLeido);
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
         });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -90,26 +81,11 @@ public class Scanner extends AppCompatActivity {
         if(result != null)
             if(result.getContents() != null){
 
+                String escaneado = result.getContents();
+                Update_List(escaneado);
+                Insert_OnebyOne(escaneado);
 
-                Map<String,Object> lote = new HashMap<>();
-                lote.put("Codigo",1);
-                lote.put("Lectura",result.getContents());
-
-                tvCodigo.setText("El Código es: \n" + result.getContents());
-                mDatabaseReference.child("Codigo").push().setValue(result.getContents());
-                mDatabaseReference.child("Lotes").push().setValue(lote);
-                tvBarCode.setText("El Código es: \n" + result.getContents());
-
-
-
-
-                //ALVARO
-                scannedQRs.add(result.getContents());
-                preference = getSharedPreferences("STORE", Context.MODE_PRIVATE);
-                preference.edit().remove("scannedQRs").commit();
-                preference.edit().putStringSet("scannedQRs", scannedQRs).commit();
-                Log.i("SCANNED-QRS", "onScan: " + scannedQRs);
-
+                tvCodigo.setText("El Código es: " + result.getContents());
             }
             else{
                 tvCodigo.setText("Error al Escanear");
@@ -123,7 +99,54 @@ public class Scanner extends AppCompatActivity {
                 case R.id.btnScanner:
                     new IntentIntegrator(Scanner.this).initiateScan();
                     break;
+                case R.id.btnEnvioLote:
+                    Insert_ByLote(listScannedQRs.getAdapter());
+                    break;
+
             }
         }
     };
+
+    void Update_List(String escaneado){
+        scannedQRs.add(escaneado);
+        //preference = getSharedPreferences("STORE", Context.MODE_PRIVATE);
+        preference.edit().remove("scannedQRs").apply();
+        preference.edit().putStringSet("scannedQRs", scannedQRs).apply();
+        Update_List();
+    }
+    void Update_List(){
+        preference = getSharedPreferences("STORE", Context.MODE_PRIVATE);
+        scannedQRs = preference.getStringSet("scannedQRs", new HashSet<String>());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                scannedQRs.toArray( new String[scannedQRs.size()]) );
+        listScannedQRs.setAdapter(adapter);
+    }
+    void Insert_OnebyOne(String escaneado){
+        mDatabaseReference.child("Codigo").push().setValue(escaneado);
+    }
+    void Insert_ByLote(ListAdapter list){
+        int cant = list.getCount();
+        Map<String,Object> lote;
+        for(int i=0 ; i<cant ; i++){
+            lote = new HashMap<>();
+            lote.put("NumeroLote", CodigoLeido);
+            lote.put("Codigo", list.getItem(i));
+            mDatabaseReference.child("Lotes").push().setValue(lote);
+        }
+        Toast.makeText(Scanner.this,
+                "Lote Enviado Correctamente",Toast.LENGTH_SHORT).show();
+
+        ClearList();
+        Update_NumLote();
+        Update_List();
+    }
+    void Update_NumLote(){
+        HashMap hmap = new HashMap();
+        hmap.put("Numero", Integer.parseInt(CodigoLeido)+1);
+        mDatabaseReference.child("Numero_Lote").updateChildren(hmap);
+    }
+    void ClearList(){
+        preference.edit().remove("scannedQRs").apply();
+    }
 }
